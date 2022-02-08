28473a9d2c308847182c3f0c7dfd457be10609ab
Lucai994 <luca.muratori.94@hotmail.it>
2022-02-08 17:53:02 +0100

    commit

--- src/components/LatestComponent.jsx
+++ src/components/LatestComponent.jsx
@@ -13,13 +13,16 @@ class LatestComponent extends Component {
                             <Col xs={12} lg={4} key={book.asin} className='my-2 d-flex border '>
                                 <img key={book.asin} className='w-50' src={book.img} />
                                 <div className='ml-3'>
-                                    <p className='font-weight-bolder'>
-                                        {book.title}
-                                    </p>
-                                    <p>
-                                        {book.price}$
-                                    </p>
-                                    <p>category : {book.category}</p>
+                                    <div className=''>
+                                        <p className='font-weight-bolder'>
+                                            {book.title}
+                                        </p>
+                                        <p>
+                                            {book.price}$
+                                        </p>
+                                        <p>category : {book.category}</p>
+                                    </div>
+                                    <div className='mt-5'><button className='btn btn-success'>Buy</button></div>
                                 </div>
 
                             </Col>
